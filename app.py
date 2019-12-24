eba51466d08324904bb73ef343cb3c134254e413
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-12-24 00:06:22 -0500

    app works  and updates databases with current balance

--- app.py
+++ app.py
@@ -7,6 +7,47 @@ from sqlalchemy.orm import relationship
 import urllib
 import json
 import plaidbalance
+import base64
+import os
+import datetime
+import plaid
+import json
+import time
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+from models import Base, BankBalance
+
+
+
+credentials_file = 'C:/Users/tonyr/Desktop/PlaidAPIFinal/credentials.json'
+
+def getplaid():
+    with open(credentials_file) as json_file:
+        data = json.load(json_file)
+        return  data['codes']['plaid']['PLAID_CLIENT_ID'], \
+                data['codes']['plaid']['PLAID_SECRET'], \
+                data['codes']['plaid']['PLAID_PUBLIC_KEY'],  \
+                data['codes']['plaid']['chase_access_token']
+    
+PLAID_CLIENT_ID,PLAID_SECRET,PLAID_PUBLIC_KEY,chase_access_token = getplaid()
+
+PLAID_ENV = os.getenv('PLAID_ENV', 'development')
+PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions')
+PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US,CA,GB,FR,ES')
+
+
+client = plaid.Client(client_id = PLAID_CLIENT_ID, secret=PLAID_SECRET,
+                      public_key=PLAID_PUBLIC_KEY, environment=PLAID_ENV, api_version='2019-05-29')
+
+def getBalance(access_token):
+    try:
+        balance_response = client.Accounts.balance.get(access_token)
+        return balance_response['accounts']
+    except plaid.errors.PlaidError as e:
+        return jsonify({'error': {'display_message': e.display_message, 'error_code': e.code, 'error_type': e.type } })
+    
 
 
 balances = getBalance(chase_access_token)
@@ -47,6 +88,6 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 balance = session.query(BankBalance).filter_by(UserID = userid).one()
 balance.KeyBalance = bankbalance
+balance.DateTime = datetime.datetime.now()
 session.add(balance)
 session.commit()
-
