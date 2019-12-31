40542faf0fa89bb4cb64d5f8dfa7fdeadea85111
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-12-31 00:12:20 -0500

    removed old app.py and renamed new version to app.py

--- app.py
+++ app.py
@@ -17,74 +17,80 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import jsonify
-from models import Base, BankBalance
+import models
+class getPlaid():
 
+    def __init__(self):
+        self.credentials_file = 'C:/Users/tonyr/Desktop/PlaidAPIFinal/credentials.json'
+        self.PLAID_CLIENT_ID = ''
+        self.PLAID_SECRET = ''
+        self.PLAID_PUBLIC_KEY = ''
+        self.chase_access_token = ''
+        self.PLAID_ENV = ''
+        self.server = ''
+        self.user = ''
+        self.password = ''
+        self.database = ''
+        self.userid = ''        
 
-
-credentials_file = 'C:/Users/tonyr/Desktop/PlaidAPIFinal/credentials.json'
-
-def getplaid():
-    with open(credentials_file) as json_file:
-        data = json.load(json_file)
-        return  data['codes']['plaid']['PLAID_CLIENT_ID'], \
-                data['codes']['plaid']['PLAID_SECRET'], \
-                data['codes']['plaid']['PLAID_PUBLIC_KEY'],  \
-                data['codes']['plaid']['chase_access_token']
+    def getCredentials(self):
+        self.PLAID_ENV = os.getenv('PLAID_ENV', 'development')
+        with open(self.credentials_file) as json_file:
+            data = json.load(json_file)
+            self.server =   data['codes']['connectionstring']['server']
+            self.user =     data['codes']['connectionstring']['user']
+            self.password = data['codes']['connectionstring']['password']
+            self.database =  data['codes']['connectionstring']['database']
+            self.userid = data['codes']['userid']['id']
+            self.PLAID_CLIENT_ID =  data['codes']['plaid']['PLAID_CLIENT_ID']
+            self.PLAID_SECRET =  data['codes']['plaid']['PLAID_SECRET']
+            self.PLAID_PUBLIC_KEY = data['codes']['plaid']['PLAID_PUBLIC_KEY']
+            self.chase_access_token =  data['codes']['plaid']['chase_access_token']
+            self.client = plaid.Client(client_id = self.PLAID_CLIENT_ID, secret=self.PLAID_SECRET,
+                            public_key=self.PLAID_PUBLIC_KEY, environment=self.PLAID_ENV, api_version='2019-05-29')
+            self.params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};"
+                            "SERVER=" + self.server + ";"
+                            "DATABASE=" + self.database + ";"
+                            "UID=" + self.user + ";"
+                            "PWD=" + self.password)
+        return self    
+class PlaidBalance():
     
-PLAID_CLIENT_ID,PLAID_SECRET,PLAID_PUBLIC_KEY,chase_access_token = getplaid()
-print("ac:" +chase_access_token)
-PLAID_ENV = os.getenv('PLAID_ENV', 'development')
-client = plaid.Client(client_id = PLAID_CLIENT_ID, secret=PLAID_SECRET,
-                      public_key=PLAID_PUBLIC_KEY, environment=PLAID_ENV, api_version='2019-05-29')
+    def __init__(self):
+        self.chasebalance = 0
 
-def getBalance(access_token):
-    try:
-        balance_response = client.Accounts.balance.get(access_token)
-        balances = balance_response['accounts']
-        for x in balances:
-            if x['name'] == 'TOTAL CHECKING':
-                bankbalance = x['balances']['available']
-                print(bankbalance)
-        return bankbalance
-        # return balance_response['accounts']
-    except plaid.errors.PlaidError as e:
-        return jsonify({'error': {'display_message': e.display_message, 'error_code': e.code, 'error_type': e.type } })
-    
-# def getTransactions()
-#   start_date = '{:%Y-%m-%d}'.format(datetime.datetime.now() + datetime.timedelta(-30))
-#   end_date = '{:%Y-%m-%d}'.format(datetime.datetime.now())
-#   try:
-#     transactions_response = client.Transactions.get(access_token, start_date, end_date)
+    def getBalance(self):
+        PlaidCredentials = getPlaid()
+        credentials = PlaidCredentials.getCredentials()
+        chase_access_token = credentials.chase_access_token
+        try:
+            balance_response = credentials.client.Accounts.balance.get(credentials.chase_access_token)
+            balances = balance_response['accounts']
+            for x in balances:
+                if x['name'] == 'TOTAL CHECKING':
+                    self.chasebalance = x['balances']['available']
+                    print(self.chasebalance)
+            return self
+        except plaid.errors.PlaidError as e:
+            return jsonify({'error': {'display_message': e.display_message, 'error_code': e.code, 'error_type': e.type } })
+class updateBankBalanceInDatabase():
 
-bankbalance = getBalance(chase_access_token)
+    def databaseUpdate(self):
+        gp = getPlaid()
+        plaidCredentials = gp.getCredentials()
+        pb = PlaidBalance()
+        pbalance = pb.getBalance() 
+        engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(plaidCredentials.params))
+        DBSession = sessionmaker(bind = engine)    
+        session = DBSession()
+        bankbalance = session.query(models.BankBalance).filter_by(UserID = plaidCredentials.userid).one()
+        bankbalance.KeyBalance = pbalance.chasebalance
+        bankbalance.DateTime = datetime.datetime.now()
+        session.add(bankbalance)
+        session.commit()
 
-def getConnection():
-    with open(credentials_file) as json_file:
-        data = json.load(json_file)
-        return  data['codes']['connectionstring']['server'], \
-                data['codes']['connectionstring']['user'], \
-                data['codes']['connectionstring']['password'],  \
-                data['codes']['connectionstring']['database']
+def main():
+    x = updateBankBalanceInDatabase()
+    x.databaseUpdate()
 
-server,user,password,database = getConnection()
-
-def getuserid():
-    with open(credentials_file) as json_file:
-        data = json.load(json_file)
-        return data['codes']['userid']['id']
-userid = getuserid() 
-
-params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};"
-                                 "SERVER=" + server + ";"
-                                 "DATABASE=" + database + ";"
-                                 "UID=" + user + ";"
-                                 "PWD=" + password)
-
-engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
-DBSession = sessionmaker(bind = engine)
-session = DBSession()
-balance = session.query(BankBalance).filter_by(UserID = userid).one()
-balance.KeyBalance = bankbalance
-balance.DateTime = datetime.datetime.now()
-session.add(balance)
-session.commit()
+main()
