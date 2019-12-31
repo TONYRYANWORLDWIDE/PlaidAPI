7a558b88f666f8c91c74263a0d9b0a1194decb07
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-12-31 00:08:17 -0500

    seperated concerns

--- app2.py
+++ app2.py
@@ -18,7 +18,7 @@ from flask import render_template
 from flask import request
 from flask import jsonify
 import models
-class plaidApp():
+class getPlaid():
 
     def __init__(self):
         self.credentials_file = 'C:/Users/tonyr/Desktop/PlaidAPIFinal/credentials.json'
@@ -27,34 +27,44 @@ class plaidApp():
         self.PLAID_PUBLIC_KEY = ''
         self.chase_access_token = ''
         self.PLAID_ENV = ''
-        self.chase_access_token = ''
         self.server = ''
         self.user = ''
         self.password = ''
         self.database = ''
-        self.userid = ''
-        self.params = ''
+        self.userid = ''        
 
-    def getplaid(self):
+    def getCredentials(self):
         self.PLAID_ENV = os.getenv('PLAID_ENV', 'development')
         with open(self.credentials_file) as json_file:
             data = json.load(json_file)
+            self.server =   data['codes']['connectionstring']['server']
+            self.user =     data['codes']['connectionstring']['user']
+            self.password = data['codes']['connectionstring']['password']
+            self.database =  data['codes']['connectionstring']['database']
+            self.userid = data['codes']['userid']['id']
             self.PLAID_CLIENT_ID =  data['codes']['plaid']['PLAID_CLIENT_ID']
             self.PLAID_SECRET =  data['codes']['plaid']['PLAID_SECRET']
             self.PLAID_PUBLIC_KEY = data['codes']['plaid']['PLAID_PUBLIC_KEY']
             self.chase_access_token =  data['codes']['plaid']['chase_access_token']
+            self.client = plaid.Client(client_id = self.PLAID_CLIENT_ID, secret=self.PLAID_SECRET,
+                            public_key=self.PLAID_PUBLIC_KEY, environment=self.PLAID_ENV, api_version='2019-05-29')
+            self.params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};"
+                            "SERVER=" + self.server + ";"
+                            "DATABASE=" + self.database + ";"
+                            "UID=" + self.user + ";"
+                            "PWD=" + self.password)
+        return self    
+class PlaidBalance():
     
-    def getPa(self):
-        self.getplaid()        
-        self.client = plaid.Client(client_id = self.PLAID_CLIENT_ID, secret=self.PLAID_SECRET,
-                        public_key=self.PLAID_PUBLIC_KEY, environment=self.PLAID_ENV, api_version='2019-05-29')
-        return self
+    def __init__(self):
+        self.chasebalance = 0
 
     def getBalance(self):
-        self.getPa()
-        chase_access_token = self.chase_access_token
+        PlaidCredentials = getPlaid()
+        credentials = PlaidCredentials.getCredentials()
+        chase_access_token = credentials.chase_access_token
         try:
-            balance_response = self.client.Accounts.balance.get(chase_access_token)
+            balance_response = credentials.client.Accounts.balance.get(credentials.chase_access_token)
             balances = balance_response['accounts']
             for x in balances:
                 if x['name'] == 'TOTAL CHECKING':
@@ -63,40 +73,24 @@ class plaidApp():
             return self
         except plaid.errors.PlaidError as e:
             return jsonify({'error': {'display_message': e.display_message, 'error_code': e.code, 'error_type': e.type } })
-
-    def getConnection(self):
-        with open(self.credentials_file) as json_file:
-            data = json.load(json_file)
-            self.server =   data['codes']['connectionstring']['server']
-            self.user =     data['codes']['connectionstring']['user']
-            self.password = data['codes']['connectionstring']['password']
-            self.database =  data['codes']['connectionstring']['database']
-            self.userid = data['codes']['userid']['id']
-        return self
-
-    def getparams(self):
-        self.getConnection()
-        self.params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};"
-                                 "SERVER=" + self.server + ";"
-                                 "DATABASE=" + self.database + ";"
-                                 "UID=" + self.user + ";"
-                                 "PWD=" + self.password)
-        return self
+class updateBankBalanceInDatabase():
 
     def databaseUpdate(self):
-        self.getBalance()    
-        self.getparams()
-        engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(self.params))
+        gp = getPlaid()
+        plaidCredentials = gp.getCredentials()
+        pb = PlaidBalance()
+        pbalance = pb.getBalance() 
+        engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(plaidCredentials.params))
         DBSession = sessionmaker(bind = engine)    
         session = DBSession()
-        bankbalance = session.query(models.BankBalance).filter_by(UserID = self.userid).one()
-        bankbalance.KeyBalance = self.chasebalance
+        bankbalance = session.query(models.BankBalance).filter_by(UserID = plaidCredentials.userid).one()
+        bankbalance.KeyBalance = pbalance.chasebalance
         bankbalance.DateTime = datetime.datetime.now()
         session.add(bankbalance)
         session.commit()
 
 def main():
-    pa = plaidApp()
-    pa.databaseUpdate()
+    x = updateBankBalanceInDatabase()
+    x.databaseUpdate()
 
 main()
